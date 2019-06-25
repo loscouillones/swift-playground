c174874882d5ff41b5422e01bd4e1deb1a00575f
warpdesign <nicolas.ramz@gmail.com>
2019-06-25 12:37:41 +0200

    ADDED: emoji project

---
+++ UI/emoji/emoji/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  emoji
+//
+//  Created by Nico on 25/06/2019.
+//  Copyright © 2019 Nico. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    let symbol: Character
+    let name: String
+    let description: String
+    let usage: String
+}
